0b90b63e41bae6d776d5daae8496743f563bc56f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-28 21:02:05 +0000

    create functions file, betty, compilation

--- _printf.c
+++ _printf.c
@@ -6,32 +6,25 @@
 */
 int _printf(const char *format, ...)
 {
-	om_t struc1[] = {
-		{'c', print_char},
-		{'s', print_str},
-		{'d', print_dec},
-		{'i', print_int},
-	};
-	int i = 0, j = 0, k = 0, a = 0;
+	om_t struc1[] = {{'c', print_char}, {'s', print_str},
+		{'d', print_dec}, {'i', print_int}, };
+	int i = 0, j = 0, a = 0;
 	va_list list;
 
 	va_start(list, format);
-
 	while (format && format[i])
 	{
 		if ((format[i] != '%') && (format[i] != '\\'))
-		{
 			_putchar(format[i]);
-		}
 		else
 		{
 			if (format[i] == '%')
 			{
 				if (format[i + 1] == '%')
-					_putchar('%);
+					_putchar('%');
 				else
 				{
-					while (j < 5)
+					while (j < 4)
 					{
 						if (format[i+1] == struc1[j].ele)
 						{
@@ -44,68 +37,10 @@ int _printf(const char *format, ...)
 					if (a == 1)
 					i++;
 				}
-			}/*Qué pasa sí no después de % no encuentra coincidencias */
-
+			}
 		}
 		i++;
-
 	}
 	return(i);
 	va_end(list);
-}
-/**
- *print_char - prints a given char
- *@list: char to be printed
- */
-
-void print_char(va_list list)
-{
-
-	_putchar(va_arg(list, int));
-}
-
-/**
- *print_str - prints a given str
- *@list:string to be printed
- */
-
-void print_str(va_list list)
-{
-	char *l;
-	int i;
-
-	i = 0;
-	l = va_arg(list, char *);
-        if (l != NULL)
-        {
-		while (l[i] != '\0')
-		{
-			_putchar(l[i]);
-			i++;
-		}
-
-	}
-	return;
-}
-/**
- *print_int - prints a given integer
- *@list: int to be printed
- */
-void print_dec(va_list list)
-{
-	int l;
-
-	l = va_arg(list, int);
-	print_number(l);
-}
-/**
- *print_int - prints a given integer
- *@list: int to be printed
- */
-void print_int(va_list list)
-{
-	int m;
-
-	m = va_arg(list, int);
-	print_number(m);
 }
--- print_number.c
+++ print_number.c
@@ -5,7 +5,6 @@
  *@n:number to be evaluated
  *Return:void
  */
-
 void print_number(int n)
 {
 
@@ -13,8 +12,10 @@ void print_number(int n)
 	{
 		_putchar('-');
 		n = n * -1;
-	}									}
+	}
 	if (n / 10)
+	{
 		print_number(n / 10);
+	}
 	_putchar(n % 10 + '0');
 }
